a65995c864fd1d6afd8c1f6fa5f44b9b34a75a02
shariff6 <shariff.mohammed.a@gmail.com>
2017-05-30 16:23:36 +0300

    latest files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,42 @@
-//import the Component code from the core Angular framework code
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
-//Component Annotation
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
+    <pies></pies>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"
+     ></task-list>
+    <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-task>
+    <new-task
+      (newTaskSender)="addTask($event)"
+    ></new-task>
   </div>
   `
 })
 
-
-//The component's class declaration holds the required data and methods to make the template HTML functional
 export class AppComponent {
-
+  public masterTaskList: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+  ];
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
+  }
 }
